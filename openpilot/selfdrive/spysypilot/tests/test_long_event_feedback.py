4004d9573da24890fde91f9936ee2e794dff88ec
SpysyWeeb <imasmartalex@gmail.com>
2026-07-23 19:14:50 -0600

    selfdrive: migrate detectors to universal event runtime

--- openpilot/selfdrive/spysypilot/tests/test_long_event_feedback.py
+++ openpilot/selfdrive/spysypilot/tests/test_long_event_feedback.py
@@ -1,4 +1,4 @@
-from openpilot.selfdrive.ui.feedback.feedbackd import manual_long_event, publish_bookmark
+from openpilot.selfdrive.ui.feedback.feedbackd import publish_bookmark
 
 
 class FakePubMaster:
@@ -11,20 +11,7 @@ class FakePubMaster:
     self.message = message.as_reader()
 
 
-def test_manual_bookmark_has_structured_ui_and_manifest_fields():
-  pm = FakePubMaster()
-  publish_bookmark(pm, manual_long_event(), source="manual")
-  bookmark = pm.message.userBookmark
-
-  assert pm.service == "userBookmark"
-  assert str(bookmark.source) == "manual"
-  assert bookmark.eventType == "manual_long_event"
-  assert bookmark.alertText1 == "Long Event Logged"
-  assert bookmark.alertText2 == "Manual bookmark"
-  assert bookmark.confidence == 1.0
-
-
-def test_generic_bookmark_remains_backward_compatible():
+def test_audio_feedback_bookmark_remains_backward_compatible():
   pm = FakePubMaster()
   publish_bookmark(pm)
   bookmark = pm.message.userBookmark
